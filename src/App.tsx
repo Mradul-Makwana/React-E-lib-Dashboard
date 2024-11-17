fc3c865821197bd135250091431b8342dafa14d7
Mradul Makwana <mradul.makwana83@gmail.com>
2024-11-17 21:05:53 +0530

    add shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 // import { useState } from "react";
 
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>Hello world</div>
+      <div className="text-black-500">Hello world</div>
+      <Button variant={"default"}>Click me</Button>
     </>
   );
 }
